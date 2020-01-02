29756fbeb13fc42cca8219920d18ff15e914b6b7
zhutingting <zhutingting@jiuyescm.com>
2020-01-02 11:00:14 +0800

    使用主题例子链接

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appName = '自定义主题';
+    return new MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightGreen[600],
+        accentColor: Colors.orange[600],
+      ),
+      home: new MyHomePage(
+        title: appName,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            '带有颜色的文本组件',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: new Theme(
+        data: Theme.of(context).copyWith(accentColor: Colors.grey),
+        child: new FloatingActionButton(
+          onPressed: null,
+          child: new Icon(Icons.computer),
+        ),
+      ),
+    );
+  }
+}
