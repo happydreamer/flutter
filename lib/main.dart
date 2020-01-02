86de6cc204487b6dca2ebbd57b1b6064b39b81f7
zhutingting <zhutingting@jiuyescm.com>
2020-01-02 14:23:53 +0800

    引用包资源

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,62 +9,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //此组件是整个应用的主组件
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-          //自定义主题
-          primarySwatch: Colors.blue),
-      home: new MyHomePage(title: 'Flutter Demo HomePage Demo'),
-    );
-  }
-}
-
-// 主页需要继承StatefulWidget
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  //标题
-  final String title;
-  //必须重写createState方法
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-//状态类必须继承state类，注意后面需要指定为<MyHomePage>
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0; //计数器
-  void _incrementCounter() {
-    //调用State类里边的setState方法来更改状态值，使得计数值加1
-    setState(() {
-      // 计数器变量，每次点击加1
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        //垂直布局
-        child: new Column(
-          //主轴居中对齐
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'you have push the button manyTimes:',
-            ),
-            new Text(
-              '$_counter', //绑定计数器的值
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      title: '使用第三方包示例',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('使用第三方包示例'),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter, //按下+号按钮调用自增函数
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
+        body: new Center(
+          child: new RaisedButton(
+            onPressed: () {
+              //指定url并发起请求
+              const url = 'https://www.baidu.com';
+              launch(url);
+            },
+            child: new Text('打开百度'),
+          ),
+        ),
       ),
     );
   }
