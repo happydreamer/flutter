1d209defa08b163262dd60790b870bfa55accda0
zhutingting <zhutingting@jiuyescm.com>
2020-01-02 15:02:39 +0800

    网络请求中的http请求

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -9,19 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //此组件是整个应用的主组件
     return new MaterialApp(
-      title: '使用第三方包示例',
+      title: 'http请求示例',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('使用第三方包示例'),
+          title: new Text('http请求示例'),
         ),
         body: new Center(
           child: new RaisedButton(
             onPressed: () {
               //指定url并发起请求
-              const url = 'https://www.baidu.com';
-              launch(url);
+              const url = 'https://httpbin.org/';
+              //向https://httpbin.org/发起get请求
+              http.get(url).then((response) {
+                print("状态: ${response.statusCode}");
+                print("正文：${response.body}");
+              });
             },
-            child: new Text('打开百度'),
+            child: new Text('发起http请求'),
           ),
         ),
       ),
