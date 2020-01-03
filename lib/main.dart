ba4cbd7d6dd5c4f88bf9b62c200faef14fabe9c2
zhutingting <zhutingting@jiuyescm.com>
2020-01-03 14:00:00 +0800

    基础列表示例

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,83 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: '凸起按钮组件',
+    final title = '基础列表示例';
+    return MaterialApp(
+      title: title,
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('凸起按钮组件'),
+          title: new Text(title),
         ),
-        body: new Center(
-          child: new RaisedButton(
-            onPressed: () {},
-            child: new Text('RaisedButton组件'),
-          ),
+        body: new ListView(
+          //添加静态数据
+          children: <Widget>[
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.phone),
+              //添加文本
+              title: Text('phone'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.airplay),
+              //添加文本
+              title: Text('airplay'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              //添加图标
+              leading: Icon(Icons.alarm),
+              //添加文本
+              title: Text('Alarm'),
+            ),
+          ],
         ),
       ),
     );
