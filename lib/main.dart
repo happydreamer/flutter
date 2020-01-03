cab4bd5b2ee61aaefed43bc98ddae14f16cbd236
zhutingting <zhutingting@jiuyescm.com>
2020-01-03 13:30:38 +0800

    带交互的图标组件

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: '图标组件',
+    title: '带交互的图标组件',
     home: new LayoutDemo(),
   ));
 }
@@ -11,10 +11,17 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: Text('图标组件'),
-      ),
-      body: new Icon(Icons.phone, color: Colors.green[500], size: 80.0),
-    );
+        appBar: new AppBar(
+          title: Text('带交互的图标组件'),
+        ),
+        body: new Center(
+          child: new IconButton(
+            icon: Icon(Icons.volume_up, size: 48.0),
+            tooltip: '按下操作',
+            onPressed: () {
+              print('按下操作');
+            },
+          ),
+        ));
   }
 }
