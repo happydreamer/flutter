3176420b0633247ecc05de3af194ef6cfa3623ab
zhutingting <zhutingting@jiuyescm.com>
2020-01-06 17:29:57 +0800

    路由主页跳转

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  //这是整个应用的主组件
+  //整个应用的主组件
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new MyHomePage(),
-      title: 'MyHomePage示例',
+      title: 'MaterialApp示例',
+      routes: {
+        '/first': (BuildContext context) => FirstPage(), //添加路由
+        '/second': (BuildContext context) => SecondPage(),
+      },
+      initialRoute: '/first', //设置初始路由界面
     );
   }
 }
 
-//这是一个可改变的Widget
+//这是一个可改变的widget
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -26,10 +31,60 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: Text('MaterialApp示例'),
+        title: Text('Material示例'),
       ),
       body: Center(
-        child: Text('----主页-----'),
+        child: Text(
+          '主页',
+          style: TextStyle(fontSize: 28.0),
+        ),
+      ),
+    );
+  }
+}
+
+//第一个路由界面
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('这是第一页'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //路由由第一个页面路由跳转到第二个页面
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text(
+            '这是第一页',
+            style: TextStyle(fontSize: 28.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('这是第二页'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //路由跳转到第一页
+            Navigator.pushNamed(context, '/first');
+          },
+          child: Text(
+            '这是第二个页面',
+            style: TextStyle(fontSize: 28.0),
+          ),
+        ),
       ),
     );
   }
