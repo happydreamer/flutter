f63ab9fb95243f5a7164b5a0b3c7432791c6b412
zhutingting <zhutingting@jiuyescm.com>
2020-01-06 19:42:40 +0800

    bottomNavigationBar底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      new MaterialApp(
-        title: 'appBar应用按钮实例',
-        home: new LayoutDemo(),
-      ),
-    );
+void main() => runApp(new MyApp());
 
-class LayoutDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('appBar应用按钮实例'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: '搜索',
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: '添加',
-            onPressed: () {},
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        body: MyHomePage(),
       ),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1; //当前选中项的索
+  final _widgetoptions = [
+    Text('Index 0:信息'),
+    Text('Index 1:通讯录'),
+    Text('Index 2:发现'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('BootomNavigationBar实例'),
+      ),
+      body: Center(
+        child: _widgetoptions.elementAt(_selectedIndex), //居中显示文本
+      ),
+      //底部导航按钮包含图标和文本
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('信息')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contacts), title: Text('通讯录')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text('发现')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped, //选择按下处理
+      ),
+    );
+  }
+
+  //选择按下处理，设置当前索引index的值
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
