5d101d99bce20c52d0ba4f2691a89665274f6b90
zhutingting <zhutingting@jiuyescm.com>
2020-01-07 13:54:47 +0800

    FlatButton扁平组件，点击后会出现水泼纹

--- lib/main.dart
+++ lib/main.dart
@@ -6,44 +6,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FloattingActionButton示例',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('FloattingActionButton示例'),
+          title: Text('FloatButton扁平按钮组件'),
         ),
         body: Center(
-          child: Text(
-            'FloattingActionButton示例',
-            style: TextStyle(fontSize: 28.0),
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(
+              'FlatButton',
+              style: TextStyle(fontSize: 24.0),
+            ),
           ),
         ),
-        floatingActionButton: new Builder(builder: (BuildContext context) {
-          return new FloatingActionButton(
-            child: const Icon(Icons.add),
-            //提示信息
-            tooltip: '请点击FloatingActionButton',
-            //前背景颜色为白色
-            foregroundColor: Colors.white,
-            //背景颜色为蓝色
-            backgroundColor: Colors.blue,
-            //未点击阴影值
-            elevation: 7.0,
-            //点击阴影值
-            highlightElevation: 14.0,
-            onPressed: () {
-              //点击回调事件，弹出提示语
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                content: new Text('你点击了FloatingActionButton'),
-              ));
-            },
-            mini: false,
-            //圆形边
-            shape: new CircleBorder(),
-            isExtended: false,
-          );
-        }),
-        floatingActionButtonLocation:
-            FloatingActionButtonLocation.centerDocked, //居中放置
       ),
     );
   }
