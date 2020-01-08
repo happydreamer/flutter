440af80e684b806b27673dace4b814d5cda79cc3
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 14:12:39 +0800

    Center居中示例

--- lib/main.dart
+++ lib/main.dart
@@ -11,93 +11,18 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //返回一个Container对象
-    Widget container = new Container(
-      //添加装饰效果
-      decoration: new BoxDecoration(
-        color: Colors.grey,
-      ),
-      //子元素指定为一个垂直水平嵌套布局的组件
-      child: new Column(
-        children: <Widget>[
-          new Row(
-            children: <Widget>[
-              //使用expanded防止内容溢出
-              new Expanded(
-                child: new Container(
-                  width: 150.0,
-                  height: 150.0,
-                  //添加边框样式
-                  decoration: new BoxDecoration(
-                    //上下左右边框设置宽度为10.0颜色为蓝灰色
-                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
-                    //上下左右边框弧度设置为8.0
-                    borderRadius:
-                        const BorderRadius.all(const Radius.circular(8.0)),
-                  ),
-                  //上下左右增加边距
-                  margin: const EdgeInsets.all(4.0),
-                  //添加图片
-                  child: new Image.asset('images/2.0x/4.jpg'),
-                ),
-              ),
-              new Expanded(
-                child: new Container(
-                  width: 150.0,
-                  height: 150.0,
-                  //添加边框样式
-                  decoration: new BoxDecoration(
-                    //上下左右边框设置宽度为10.0颜色为蓝灰色
-                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
-                    //上下左右边框弧度设置为8.0
-                    borderRadius:
-                        const BorderRadius.all(const Radius.circular(8.0)),
-                  ),
-                  //上下左右增加边距
-                  margin: const EdgeInsets.all(4.0),
-                  //添加图片
-                  child: new Image.asset('images/2.0x/5.jpg'),
-                ),
-              ),
-            ],
-          ),
-          new Row(
-            children: <Widget>[
-              new Expanded(
-                child: new Container(
-                  width: 150.0,
-                  height: 150.0,
-                  decoration: new BoxDecoration(
-                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
-                    borderRadius:
-                        const BorderRadius.all(const Radius.circular(8.0)),
-                  ),
-                  margin: const EdgeInsets.all(4.0),
-                  child: new Image.asset('images/2.0x/6.jpg'),
-                ),
-              ),
-              new Expanded(
-                child: new Container(
-                  width: 150.0,
-                  height: 150.0,
-                  decoration: new BoxDecoration(
-                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
-                    borderRadius:
-                        const BorderRadius.all(const Radius.circular(8.0)),
-                  ),
-                  margin: const EdgeInsets.all(4.0),
-                  child: new Image.asset('images/2.0x/7.jpg'),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
     return new Scaffold(
-      appBar: AppBar(
-        title: new Text('container容器示例'),
+      appBar: new AppBar(
+        title: new Text('Center居中示例'),
       ),
-      body: container,
+      body: new Center(
+        child: new Text(
+          'Hello Flutter',
+          style: TextStyle(
+            fontSize: 36.0,
+          ),
+        ),
+      ),
     );
   }
 }
