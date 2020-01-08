72f7a67c2b083fb3f3a4c552916c4742165291e2
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 14:58:56 +0800

    ROW水平布局示例

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,37 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'ROW水平布局示例',
+      home: new LayoutDemo(),
+    ),
+  );
+}
+
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Align对齐布局示例'),
+        title: new Text('ROW水平布局示例'),
       ),
-      body: new Stack(
+      body: new Row(
         children: <Widget>[
-          //左上角
-          new Align(
-            alignment: new FractionalOffset(0.0, 0.0),
-            child: new Image.asset(
-              'images/2.0x/1.jpg',
-              width: 100.0,
-              height: 100.0,
-            ),
+          new Expanded(
+            child: new Text('左侧文本', textAlign: TextAlign.center),
           ),
-          //右上角
-          new Align(
-            alignment: FractionalOffset(1.0, 0.0),
-            child: new Image.asset(
-              'images/2.0x/2.jpg',
-              width: 100.0,
-              height: 100.0,
-            ),
+          new Expanded(
+            child: new Text('中间文本', textAlign: TextAlign.center),
           ),
-          //水平垂直反向居中
-          new Align(
-            alignment: FractionalOffset.center,
-            child: new Image.asset(
-              'images/2.0x/4.jpg',
-              width: 100.0,
-              height: 100.0,
+          new Expanded(
+            child: new FittedBox(
+              fit: BoxFit.contain,
+              child: const FlutterLogo(),
             ),
-          ),
-          new Align(
-            alignment: FractionalOffset.bottomLeft,
-            child: new Image.asset(
-              'images/2.0x/5.jpg',
-              width: 100.0,
-              height: 100.0,
-            ),
-          ),
-          //右下角
-          new Align(
-            alignment: FractionalOffset.bottomRight,
-            child: new Image.asset(
-              'images/2.0x/6.jpg',
-              width: 100.0,
-              height: 100.0,
-            ),
-          ),
+          )
         ],
       ),
     );
   }
-}
-
-void main() {
-  runApp(
-    new MaterialApp(
-      title: 'Align对齐方式显示',
-      home: new LayoutDemo(),
-    ),
-  );
 }
