84362ee39b8f901ee775bbd3312ca9ef145fb8ad
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 17:51:58 +0800

    LimitedBox限定宽高示例

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,22 @@ class LayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('OverFlowBox溢出父容器显示示例'),
+        title: new Text('LimitedBox限定宽高示例'),
       ),
-      body: Container(
-        color: Colors.green,
-        width: 200.0,
-        height: 200.0,
-        padding: const EdgeInsets.all(50.0),
-        child: OverflowBox(
-          alignment: Alignment.topLeft,
-          maxHeight: 400.0,
-          maxWidth: 400.0,
-          child: Container(
-            color: Colors.blueGrey,
-            width: 300.0,
-            height: 300.0,
+      body: Row(
+        children: <Widget>[
+          Container(
+            color: Colors.grey,
+            width: 100.0,
           ),
-        ),
+          LimitedBox(
+            maxWidth: 150.0,
+            child: Container(
+              color: Colors.lightBlue,
+              width: 250.0,
+            ),
+          )
+        ],
       ),
     );
   }
@@ -29,7 +28,7 @@ class LayoutDemo extends StatelessWidget {
 
 void main() {
   runApp(new MaterialApp(
-    title: 'overflowbox',
+    title: 'LimitedBox限定宽高示例',
     home: new LayoutDemo(),
   ));
 }
