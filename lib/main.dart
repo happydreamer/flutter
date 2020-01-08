63b03d62b83c64b489929b0ffad2ca5e6a00d015
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 16:36:25 +0800

    IndexedStack根据index显示第index的child，因为index为0时，不显示，index===1时显示

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new MaterialApp(
-      title: '层叠定位布局示例',
-      home: new MyApp(),
-    ),
-  );
+  runApp(new MaterialApp(
+    title: 'IndexedStateStack布局示例',
+    home: new MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var stack = new IndexedStack(
+      index: 1, //设置索引值
+      alignment: const FractionalOffset(0.2, 0.2),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new AssetImage('images/2.0x/4.jpg'),
+          radius: 100.0,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.black38,
+          ),
+          child: new Text(
+            '盛世美颜我姐姐',
+            style: new TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('层叠定位布局示例'),
+        title: new Text('Stack层叠布局示例'),
       ),
       body: new Center(
-        child: new Stack(
-          children: <Widget>[
-            //添加网络图片
-            new Image.network(
-                'http://pic27.nipic.com/20130304/7368717_232014122100_2.jpg'),
-            //设置定位布局
-            new Positioned(
-              bottom: 50.0,
-              right: 50.0,
-              child: new Text(
-                'hi Flutter',
-                style: TextStyle(
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'serif',
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
+        child: stack,
       ),
     );
   }
