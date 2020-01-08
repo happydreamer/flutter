aff232b66639d769279f5d18e13e372d06da32ed
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 14:07:38 +0800

    container容器示例，row行显示，expanded显示成一行

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:flutetr/cupertino.dart';
 
-void mian() => runApp(new MyApp());
+void main() => runApp(
+      new MaterialApp(
+        title: 'Container容器示例',
+        home: new LayoutDemo(),
+      ),
+    );
 
-class MyApp extends StatelessWidget {
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CupertinoActivityIndicator示例',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('CupertinoActivityIndicator示例'),
-        ),
-        body: Center(
-          child: CupertinoActivityIndicator(
-            radius: 60.0,
-          ),
-        ),
+    //返回一个Container对象
+    Widget container = new Container(
+      //添加装饰效果
+      decoration: new BoxDecoration(
+        color: Colors.grey,
       ),
+      //子元素指定为一个垂直水平嵌套布局的组件
+      child: new Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              //使用expanded防止内容溢出
+              new Expanded(
+                child: new Container(
+                  width: 150.0,
+                  height: 150.0,
+                  //添加边框样式
+                  decoration: new BoxDecoration(
+                    //上下左右边框设置宽度为10.0颜色为蓝灰色
+                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                    //上下左右边框弧度设置为8.0
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  //上下左右增加边距
+                  margin: const EdgeInsets.all(4.0),
+                  //添加图片
+                  child: new Image.asset('images/2.0x/4.jpg'),
+                ),
+              ),
+              new Expanded(
+                child: new Container(
+                  width: 150.0,
+                  height: 150.0,
+                  //添加边框样式
+                  decoration: new BoxDecoration(
+                    //上下左右边框设置宽度为10.0颜色为蓝灰色
+                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                    //上下左右边框弧度设置为8.0
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  //上下左右增加边距
+                  margin: const EdgeInsets.all(4.0),
+                  //添加图片
+                  child: new Image.asset('images/2.0x/5.jpg'),
+                ),
+              ),
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: new BoxDecoration(
+                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/2.0x/6.jpg'),
+                ),
+              ),
+              new Expanded(
+                child: new Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: new BoxDecoration(
+                    border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: new Image.asset('images/2.0x/7.jpg'),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('container容器示例'),
+      ),
+      body: container,
     );
   }
 }
