7b69372d86d7b120630c433df1f907065a36d19b
zhutingting <zhutingting@jiuyescm.com>
2020-01-08 15:49:48 +0800

    Stack布局示例Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      title: 'FittedBox布局示例',
-      home: new LayoutDemo(),
+      title: 'Stack布局示例Alignment',
+      home: new MyApp(),
     ),
   );
 }
 
-class LayoutDemo extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('FittedBox布局示例'),
-      ),
-      body: new Container(
-        color: Colors.grey,
-        width: 250.0,
-        height: 250.0,
-        child: new FittedBox(
-          fit: BoxFit.contain, //改变填充属性
-          alignment: Alignment.topLeft,
-          child: new Container(
-            color: Colors.deepOrange,
-            child: new Text('缩放布局'),
+    var stack = new Stack(
+      //子组件左上角对齐
+      alignment: Alignment.topLeft,
+      children: <Widget>[
+        //底部添加一个头像
+        new CircleAvatar(
+          backgroundImage: new AssetImage('images/2.0x/5.jpg'),
+          radius: 100.0,
+        ),
+        //上面加一个容器，容器中再放一段文本
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.black38,
+          ),
+          child: new Text(
+            '我是超级飞侠',
+            style: new TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
+      ],
+    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Stack层叠布局示例'),
+      ),
+      body: new Center(
+        child: stack,
       ),
     );
   }
