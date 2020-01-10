9290dc2223223f2e332f2c98c058f2c8a0f2a7a9
zhutingting <zhutingting@jiuyescm.com>
2020-01-10 13:54:44 +0800

    Baseline基准线布局示例

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,46 @@ class LayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('TransForm矩阵转换示例'),
+        title: new Text('Baseline基准线布局示例'),
       ),
-      body: new Center(
-        child: Container(
-          color: Colors.grey,
-          child: Transform(
-            alignment: Alignment.topRight,
-            transform: Matrix4.rotationZ(0.3),
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              color: const Color(0xFFE8581C),
-              child: const Text('TransForm矩阵转换'),
+      body: new Row(
+        //水平等间距排列组件
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          //设置基准线
+          new Baseline(
+            baseline: 80.0,
+            //对齐字符底部水平线
+            baselineType: TextBaseline.alphabetic,
+            child: new Text(
+              '赵丽颖',
+              style: new TextStyle(
+                fontSize: 18.0,
+                textBaseLine: TextBaseLine.alphabetic,
+              ),
             ),
           ),
-        ),
+          new Baseline(
+            baseline: 80.0,
+            baselineType: TextBaseline.alphabetic,
+            child: new Container(
+              width: 40.0,
+              height: 40.0,
+              color: Colors.green,
+            ),
+          ),
+          new Baseline(
+            baseline: 80.0,
+            baselineType: TextBaseline.alphabetic,
+            child: new Text(
+              '冯绍峰',
+              style: new TextStyle(
+                fontSize: 26.0,
+                textBaseLine: TextBaseLine.alphabetic,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -28,7 +53,7 @@ class LayoutDemo extends StatelessWidget {
 void main() {
   runApp(
     new MaterialApp(
-      title: 'TransForm矩阵转换',
+      title: 'BaseLine基准线布局示例',
       home: new LayoutDemo(),
     ),
   );
