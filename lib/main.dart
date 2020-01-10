3f35e2d2952a284253c4e4863b3f86f6238e6662
zhutingting <zhutingting@jiuyescm.com>
2020-01-10 17:47:51 +0800

    导航页面示例

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: '滑动删除示例',
-    home: new MyApp(),
+    title: '导航页面示例',
+    home: new FirstScreen(),
   ));
 }
 
-class MyApp extends StatelessWidget {
-  //构建30条列表数据
-  List<String> items = new List<String>.generate(30, (i) => '列表项${i + 1}');
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('滑动删除示例'),
+        title: new Text('导航页面示例'),
       ),
-      //构建列表
-      body: new ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          //构建列表
-          //提取出被删除的项
-          final item = items[index];
-          //返回一个可以被删除的列表项
-          return new Dismissible(
-            key: new Key(item),
-            //被删除回调
-            onDismissed: (direction) {
-              items.removeAt(index);
-              //底部弹出消息提示当前项被删除了
-              Scaffold.of(context)
-                  .showSnackBar(new SnackBar(content: new Text("$item 被删除了")));
-            },
-            child: new ListTile(
-              title: new Text('$item'),
-            ),
-          );
-        },
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text('查看商品详情页面'),
+          onPressed: () {
+            Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new SecondScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('导航页面示例'),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: new Text('返回页面'),
+        ),
       ),
     );
   }
