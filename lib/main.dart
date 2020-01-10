19c5399d74f16ab2cd6337e34e453c666232e34d
zhutingting <zhutingting@jiuyescm.com>
2020-01-10 11:42:16 +0800

    TransForm矩阵转换

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: 'Table表格布局示例',
-    home: new MyApp(),
-  ));
-}
-
-class MyApp extends StatelessWidget {
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Table表格示例'),
+        title: new Text('TransForm矩阵转换示例'),
       ),
       body: new Center(
-        child: Table(
-          //设置表格有多少列，并指定列宽
-          columnWidths: const <int, TableColumnWidth>{
-            0: FixedColumnWidth(100.0),
-            1: FixedColumnWidth(40.0),
-            2: FixedColumnWidth(80.0),
-            3: FixedColumnWidth(80.0),
-          },
-          //设置表格边框样式
-          border: TableBorder.all(
-              color: Colors.black38, width: 2.0, style: BorderStyle.solid),
-          children: const <TableRow>[
-            //添加第一行数据
-            TableRow(
-              children: <Widget>[
-                Text('姓名'),
-                Text('性别'),
-                Text('年龄'),
-                Text('身高'),
-              ],
+        child: Container(
+          color: Colors.grey,
+          child: Transform(
+            alignment: Alignment.topRight,
+            transform: Matrix4.rotationZ(0.3),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              color: const Color(0xFFE8581C),
+              child: const Text('TransForm矩阵转换'),
             ),
-            //添加第二行数据
-            TableRow(
-              children: <Widget>[
-                Text('朱亭亭'),
-                Text('女'),
-                Text('18'),
-                Text('160'),
-              ],
-            ),
-            TableRow(
-              children: <Widget>[
-                Text('吴琪'),
-                Text('女'),
-                Text('17'),
-                Text('175'),
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'TransForm矩阵转换',
+      home: new LayoutDemo(),
+    ),
+  );
 }
